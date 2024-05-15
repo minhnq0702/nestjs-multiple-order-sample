49cc0799a000d6ab63da068664ea7d9eb8c10ee4
MingNe <minhnq.0702@gmail.com>
2024-05-15 15:07:28 +0700

    fix test

--- test/unit-tests/controllers/auth.controller.spec.ts
+++ test/unit-tests/controllers/auth.controller.spec.ts
@@ -1,21 +1,22 @@
 import { AuthController } from '@module/auth/auth.controller';
 import { UsersModule } from '@module/users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from '@svc/auth.service';
+import { jwtConfig } from '../../../src/config/jwt.config';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
-  const _configModule = ConfigModule.forRoot({
-    envFilePath: ['.env', '.env.development'],
-    isGlobal: true, // * Make the configuration global
-    cache: true, // * Enable configuration caching
-  });
+  // const _configModule = ConfigModule.forRoot({
+  //   envFilePath: ['.env', '.env.development'],
+  //   isGlobal: true, // * Make the configuration global
+  //   cache: true, // * Enable configuration caching
+  // });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [UsersModule, _configModule],
+      imports: [UsersModule, JwtModule.registerAsync(jwtConfig())],
       controllers: [AuthController],
       providers: [AuthService],
     }).compile();
--- test/unit-tests/svc/auth.service.spec.ts
+++ test/unit-tests/svc/auth.service.spec.ts
@@ -1,21 +1,22 @@
 import { UsersModule } from '@module/users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from '@svc/auth.service';
 import { UsersService } from '@svc/users.service';
+import { jwtConfig } from '../../../src/config/jwt.config';
 
 describe('AuthService', () => {
   let service: AuthService;
 
-  const _configModule = ConfigModule.forRoot({
-    envFilePath: ['.env', '.env.development'],
-    isGlobal: true, // * Make the configuration global
-    cache: true, // * Enable configuration caching
-  });
+  // const _configModule = ConfigModule.forRoot({
+  //   envFilePath: ['.env', '.env.development'],
+  //   isGlobal: true, // * Make the configuration global
+  //   cache: true, // * Enable configuration caching
+  // });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [UsersModule, _configModule],
+      imports: [UsersModule, JwtModule.registerAsync(jwtConfig())],
       providers: [AuthService, UsersService],
     }).compile();
 
